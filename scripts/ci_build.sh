a74e27d561c4363ee26f3ff01a8c9d1394714e3b
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2015-10-21 17:20:31 -0700

    adjust ci script

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
+set -e
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Some things depend on HOME and it may not be set. We should fix those things, but until then, we just patch a value in
+if [ -z "$HOME" ]; then
+    export HOME=$SCRIPT_DIR/../artifacts/home
+
+    [ ! -d "$HOME" ] || rm -Rf $HOME
+    mkdir -p $HOME
+fi
 
 if [[ "$(uname)" == "Linux" ]]; then
     $SCRIPT_DIR/dockerbuild.sh debian $@
 else
     $SCRIPT_DIR/../build.sh $@
 fi
-
-ret_code=$?
-exit $ret_code
-
